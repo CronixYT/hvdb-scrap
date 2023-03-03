9f6ea1929533f702b128e894a8040fb228fe1cce
CronixYT <39212842+CronixYT@users.noreply.github.com>
2023-03-04 04:37:01 +0700

    Berak 2

--- hvdb.js
+++ hvdb.js
@@ -13,6 +13,8 @@ const browserStart = async (account, { pageNum, pageSize }) => {
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
   );
 
+  await page.setDefaultNavigationTimeout(0);
+
   // Check cookie
   await checkLogin(page, cookie, account);
 
